5f07a8a7cc876d19ab792b6c9252eaf054fad361
Sandeep <sandeep@ascinatetech.com>
2025-07-14 17:33:03 +0530

    Initial commit with sidebar new 3

--- src/app/component/Sidebar.jsx
+++ src/app/component/Sidebar.jsx
@@ -24,7 +24,7 @@ function Sidebar() {
   useEffect(() => {
     async function fetchCategories() {
       try {
-        const res = await fetch('https://ascinate.in/demo/portfolio/category/api');
+        const res = await fetch('https://ascinate.in/demo/portfolio/api/category');
         const data = await res.json();
   
         if (data.categories) {
