46a52cebe9b40df30fb8caf8ead7d0c70cc6986e
Sandeep <sandeep@ascinatetech.com>
2025-07-15 11:01:34 +0530

    todays changes 1

--- src/app/component/Sidebar.jsx
+++ src/app/component/Sidebar.jsx
@@ -1,19 +1,15 @@
 'use client';
 
-import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { PiBracketsCurlyBold } from "react-icons/pi";
 import { LuMonitorPlay } from "react-icons/lu";
 import { RiBrush2Line } from "react-icons/ri";
 import { RiMagicLine } from "react-icons/ri";
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
 
-function Sidebar() {
-  const pathname = usePathname();
+function Sidebar({ onCategorySelect }) {
   const [categories, setCategories] = useState([]);
 
-  // Icon fallback array to visually differentiate categories
   const icons = [
     <PiBracketsCurlyBold />,
     <LuMonitorPlay />,
@@ -26,10 +22,9 @@ function Sidebar() {
       try {
         const res = await fetch('https://ascinate.in/demo/portfolio/api/category');
         const data = await res.json();
-  
+
         if (data.categories) {
           setCategories(data.categories);
-            console.log('fetching categories:',data.categories);
         }
       } catch (error) {
         console.error('Error fetching categories:', error);
@@ -42,24 +37,23 @@ function Sidebar() {
   return (
     <aside className="slide-bar float-start">
       <div className="inside-content">
-        <Link href="/" className="logo-sl d-block">
+        <div className="logo-sl d-block">
           <Image alt="logo" width={200} height={200} src="/nw-logo.png" />
-        </Link>
+        </div>
 
         <div className="menu-05">
           <ul>
             {categories.map((category, index) => {
-              const isActive = pathname === `/${category.slug}`;
-              const icon = icons[index % icons.length]; // rotate icons if more than 4
-
+              const icon = icons[index % icons.length];
               return (
                 <li key={category.id}>
-                  <Link
-                    href={`/${category.slug}`}
-                    className={`d-flex align-items-center ${isActive ? 'active' : ''}`}>
+                  <button
+                    onClick={() => onCategorySelect(category)}
+                    className="d-flex align-items-center border-0 bg-transparent text-start w-100 text-white"
+                  >
                     {icon}
                     <span>{category.name}</span>
-                  </Link>
+                  </button>
                 </li>
               );
             })}
