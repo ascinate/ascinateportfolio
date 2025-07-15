8079ba6d7ad8cde4be6300ee9ca33b17d67190fc
Sandeep <sandeep@ascinatetech.com>
2025-07-15 16:30:52 +0530

    this is sidebar updates

--- src/app/component/Sidebar.jsx
+++ src/app/component/Sidebar.jsx
@@ -45,11 +45,12 @@ function Sidebar({ onCategorySelect }) {
           <ul>
             {categories.map((category, index) => {
               const icon = icons[index % icons.length];
+              const isActive = pathname === `/${category.slug}`;
               return (
                 <li key={category.id}>
                   <button
                     onClick={() => onCategorySelect(category)}
-                    className="d-flex align-items-center border-0 bg-transparent text-start w-100 text-white"
+                   className={`d-flex align-items-center ${isActive ? 'active' : ''}`}
                   >
                     {icon}
                     <span>{category.name}</span>
